78feaf703d885e2549de033f7ee1b4195bbe3cbd
gumi <suzhou.gumi@gmail.com>
2024-08-12 16:35:05 +0800

    feat: functionComponent

--- tests/test-react/index.jsx
+++ tests/test-react/index.jsx
@@ -1,16 +1,17 @@
 import React from "../../dist/node_modules/react";
 import ReactDOM from "../../dist/node_modules/react-dom";
 
-const jsx = (
-  <div>
-    <span>own-react</span>
-  </div>
-);
+function App() {
+  return (
+    <div>
+      <span>own-react</span>
+    </div>
+  );
+}
 
 const root = document.querySelector("#root");
 
-ReactDOM.createRoot(root).render(jsx);
+ReactDOM.createRoot(root).render(<App />);
 
 console.log(React);
-console.log(jsx);
 console.log(ReactDOM);
